13e875a69b16997456d64d9911ef8a6f0138ecb4
Ilya Shibaev <hausdr@gmail.com>
2022-03-13 14:35:22 +0300

    добавлен prestine

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import {creaateOffers} from './create-objects.js';
 import {createCard, mapCanvas} from './generator.js';
+import {formValidation, mainForm} from './validation.js'
 
 let offers = creaateOffers(4);
 
@@ -7,4 +8,10 @@ const similarListFragment = document.createDocumentFragment();
 
 offers.forEach((data) => similarListFragment.append(createCard(data)));
 
-mapCanvas.append(similarListFragment)
+mapCanvas.append(similarListFragment);
+
+window.onload = function() {
+
+  formValidation(mainForm);
+
+};
---
+++ js/validation.js
@@ -0,0 +1,25 @@
+export let mainForm = document.querySelector(".ad-form");
+
+export let formValidation = (form) => {
+
+  var config = {
+    classTo: 'ad-form__element',
+    errorClass: 'has-danger',
+    successClass: 'has-success',
+    errorTextParent: 'ad-form__element',
+    errorTextTag: 'div',
+    errorTextClass: 'text-help'
+};
+
+  // create the pristine instance
+  let pristine = new Pristine(form, config);
+
+  form.addEventListener('submit', function (e) {
+     e.preventDefault();
+
+     // check if the form is valid
+     let valid = pristine.validate(); // returns true or false
+
+
+  });
+}
